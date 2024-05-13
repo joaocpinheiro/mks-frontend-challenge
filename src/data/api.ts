b06de96621ab87d95b8b3603639dd2e2bfead4d9
Joao Carlos <joaocpinheiro03@gmail.com>
2024-05-13 09:37:49 -0300

    feat: deploy

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,7 +1,7 @@
 import { env } from '@/env'
 
 export async function api(path: string, init?: RequestInit) {
-  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const baseUrl = env?.NEXT_PUBLIC_API_BASE_URL
   const apiPrefix = '/api'
   const url = new URL(apiPrefix.concat(path), baseUrl)
   return fetch(url, init)
